3d7ef51c05b37a22136b7640035fceb833ec9de4
chris <cjr47@cornell.edu>
2025-01-22 13:29:14 -0500

    Added form validation for the create experiment forms to validate if > 5 characters are used in certian inputs

--- portal/server/static/js/create_exp_form.js
+++ portal/server/static/js/create_exp_form.js
@@ -36,7 +36,71 @@ function requiredInput(isRequired){
         })
 }
 
+function validateForm(){
+    let validForm = true
+    let validTitle = min5CharacterAnswer('experimentTitle')
+    let validDescription = min5CharacterAnswer('description')
+    let validGoal = min5CharacterAnswer('goal')
 
+    if(validTitle == false){
+        $('#experimentTitle-message').remove()
+        $('#experimentTitle').after("<p id='experimentTitle-message' class='text-danger'>*Must be greater than 5 characters long</p>")
+        validForm = false
+    }else{
+        $('#experimentTitle-message').remove()
+    }
+
+    if(validDescription == false && document.getElementById('description').required == true){
+        $('#description-message').remove()
+        $('#description').after("<p id='description-message' class='text-danger'>*Must be greater than 5 characters long</p>")
+        validForm = false
+    }else if(validDescription == true && document.getElementById('description').required == true){
+        $('#description-message').remove()
+    }else if(document.getElementById('description').required == false){
+        validDescription= true
+    }
+
+    if(validGoal == false){
+        $('#goal-message').remove()
+        $('#goal').after("<p id='goal-message' class='text-danger'>*Must be greater than 5 characters long</p>")
+        validForm = false
+    }else{
+        $('#goal-message').remove()
+    }
+    console.log(`the form is valid: ${validForm}`)
+    if(validTitle == true && validDescription == true && validGoal == true){
+        validForm = true
+        $('#canonicalSubmit').prop('disabled', false)
+        $('#nonCanonicalSubmit').prop('disabled', false)
+    }
+
+    if(validForm == false){
+        $('#canonicalSubmit').prop('disabled', true)
+        $('#nonCanonicalSubmit').prop('disabled', true)
+    }
+}
+
+function min5CharacterAnswer(inputId){
+    let input = $(`#${inputId}`)
+    let inputVal = $(`#${inputId}`).val()
+    
+    console.log(`${inputId} length= ${inputVal.length}`)
+    if(inputVal.length <= 5 ){
+        return false
+    }else{
+        return true
+    }
+}
 
 canonicalExperiment()
 nonCanonicalExperiment()
+$('textarea').each((i,textarea)=>{
+    $(textarea).on('input', () => {
+        validateForm()
+    })
+})
+$('input[type=text]').each((i,input)=>{
+    $(input).on('input', () => {
+        validateForm()
+    })
+})
