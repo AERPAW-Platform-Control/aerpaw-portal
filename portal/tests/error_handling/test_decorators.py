8fbce3574b60343f03c7973c5ca3796d4b885273
chris <cjr47@cornell.edu>
2025-04-09 10:57:46 -0400

    edited test_decorators.py

--- portal/tests/error_handling/test_decorators.py
+++ portal/tests/error_handling/test_decorators.py
@@ -1,12 +1,20 @@
-import os
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-class TestErrorHandlingDecorator(TestCase):
+class ErrorHandlingDecoratorTest(TestCase):
     
     @classmethod
-    def setUp(self):
-        return super().setUp()
+    def setUpTestData(cls):
+        test_user1 = User.objects.create_user(username='tester1@gmail.com', password='test123!')
+        test_user2 = User.objects.create_user(username='tester2@ncsu.edu', password='test456!')
+
+    """ def setUp(self):
+        return super().setUp() """
     
     def test_experiment_detail_view_error(self):
-        self.client.get('/experiments/detial')
+        self.client.get(reverse('experiment_detail'))
+
+    
